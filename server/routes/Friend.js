9aabc5559c42797f3949d6dea14d7d06325b6aa0
Antoine Plante <anplante2@gmail.com>
2021-05-02 14:54:48 -0400

    Fixed game invite

--- server/routes/Friend.js
+++ server/routes/Friend.js
@@ -153,6 +153,7 @@ app.post('/friend/game', authorized(), cors(), [
   body('opponent').isString().trim().isAlphanumeric(),
 ], async (req, res) => {
   const errors = validationResult(req)
+  console.log(req.body)
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
